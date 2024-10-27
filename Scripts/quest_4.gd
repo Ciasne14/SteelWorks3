c98992e02e250ff47055907498b6145dc2724bdc
Mateusz0lszewski <mateuszolszewski@opoczta.pl>
2024-10-27 15:54:15 +0100

    poprawki

--- Scripts/quest_4.gd
+++ Scripts/quest_4.gd
@@ -12,7 +12,7 @@ func set_random_values():
 	for button in buttons:
 		var random_number = randi() % 100 + 1
 		button.disabled = false #
-		button.size = Vector2(int(randf_range(10, 100)), int(randf_range(10, 100)))
+		button.size = Vector2(int(randf_range(20, 100)), int(randf_range(20, 100)))
 		button.text = str(random_number)
 		if(random_number > max_val):
 			max_val = random_number
