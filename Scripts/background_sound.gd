41a2e08f4ccbcc50a72db69b486b2ce4ed7fb33f
Mateusz0lszewski <mateuszolszewski@opoczta.pl>
2024-10-27 03:05:36 +0100

    guziki i dźwięki

--- Scripts/background_sound.gd
+++ Scripts/background_sound.gd
@@ -1,6 +1,6 @@
 extends AudioStreamPlayer
 
 func _on_finished() -> void:
-	var new_audio_stream = load("res://Images/background_at_start.mp3")
+	var new_audio_stream = load("res://Sounds/background_at_start.mp3")
 	$".".stream = new_audio_stream
 	$".".play()
